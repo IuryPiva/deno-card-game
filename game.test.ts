bf5411740fc6a193e4bc9932f9343ead8ca7c3e0
Iury Piva <iurypiva@gmail.com>
2022-01-22 20:06:57 -0300

    implement drawable game

--- game.test.ts
+++ game.test.ts
@@ -3,9 +3,9 @@ import {
   assertEquals,
   assertThrows,
 } from "https://deno.land/std@0.122.0/testing/asserts.ts";
-import { Game } from "./game.ts";
+import { DrawableGame, Game } from "./game.ts";
 
-Deno.test("Game.constructor()", () => {
+Deno.test("Game()", () => {
   Array.from({ length: 10 }, (_, i) => {
     const playerCount = i + 2;
     const game = new Game(playerCount);
@@ -29,9 +29,42 @@ Deno.test("Game.constructor()", () => {
     Error,
     "You need at least 2 players to play this game",
   );
+
   assertThrows(
     () => new Game(53),
     Error,
     "Maximum 52 players could play this game",
   );
+});
+
+Deno.test("DrawableGame", () => {
+  const baselineDrawTitle0 = `------------
+DENO-UR-CARD
+------------`;
+  assertEquals(
+    baselineDrawTitle0,
+    DrawableGame.drawTitle(0),
+  );
+
+  const baselineDrawTitle30 = `------------------------------
+         DENO-UR-CARD
+------------------------------`;
+  assertEquals(
+    baselineDrawTitle30,
+    DrawableGame.drawTitle(30),
+  );
+
+  const firstRoundBaseline = [
+    "--------------",
+    " DENO-UR-CARD",
+    "--------------",
+    "",
+    "0️⃣ 0️⃣ 0️⃣   0️⃣ 0️⃣ 0️⃣ ",
+    "              ",
+    "  😊      😊  ",
+    "",
+    "Cards left: 26",
+  ].join("\n");
+  const game = new DrawableGame();
+  assertEquals(firstRoundBaseline, game.draw());
 });
--- game.ts
+++ game.ts
@@ -1,10 +1,19 @@
-import { deal, shuffle } from "./deck.ts";
-import { Player } from "./player.ts";
+import { Card, Deck, DrawableCard } from "./deck.ts";
+import { Drawable } from "./drawable.ts";
+import { DrawablePlayer, Player } from "./player.ts";
 
 export class Game {
-  players: Player[];
+  static title = "DENO-UR-CARD";
+  finished = false;
 
-  constructor(playerCount: number) {
+  players: Player[] = [];
+  cardsPlayedPerRound: Card[][] = [];
+
+  constructor(
+    playerCount = 2,
+    public PlayerType = Player,
+    public CardType = Card,
+  ) {
     if (playerCount > 52) {
       throw new Error("Maximum 52 players could play this game");
     }
@@ -12,14 +21,18 @@ export class Game {
     if (playerCount < 2) {
       throw new Error("You need at least 2 players to play this game");
     }
+    console.log(PlayerType.name);
+    const deck = new Deck(this.CardType);
 
-    const deck = shuffle();
-    const dealer = deal(deck, playerCount);
+    // I ALWAYS SHUFFLE TWICE IRL
+    deck.shuffle();
+    deck.shuffle();
 
-    this.players = Array.from(
-      { length: playerCount },
-      () => new Player(dealer.next().value),
-    );
+    const dealer = deck.deal(playerCount);
+
+    for (const hand of dealer) {
+      this.players.push(new this.PlayerType(hand));
+    }
   }
 
   /**
@@ -27,22 +40,130 @@ export class Game {
    */
   round() {
     if (this.players.some((player) => player.handSize == 0)) {
-      throw new Error("A player is out of cards!");
+      return this.finishGame();
     }
 
-    const playedCards = this.players.map((player) => player.play());
+    const playedCards = this.players.map((player) => player.play()!);
+    this.cardsPlayedPerRound.push(playedCards);
 
-    const winner = playedCards.reduce((winner, card, i) => {
-      if (card && card.value > winner.card.value) return { card, i };
-      return winner;
-    }, { i: -1, card: { value: 0 } });
+    const winnerCard = this.getWinnerCard(playedCards);
 
     this.players.forEach((player, i) => {
-      if (i == winner.i) player.handleVictory();
+      if (i == winnerCard.i) player.handleVictory();
       else player.handleLoss();
     });
   }
 
+  fastForward(cb: () => void) {
+    setTimeout(() => {
+      cb();
+      this.round();
+      if (!this.finished) this.fastForward(cb);
+    }, 100);
+  }
+
+  getWinnerCard(cards: Card[]): { card: Card; i: number } {
+    return cards.reduce((winner, card, i) => {
+      if (card && card.value > winner.card.value) return { card, i };
+      return winner;
+    }, { i: -1, card: { value: 0 } });
+  }
+
+  finishGame() {
+    this.finished = true;
+
+    const winner = this.players.reduce((winner, { points }, i) => {
+      if (points > winner.points) return { points, i: [i] };
+      if (points == winner.points) winner.i.push(i);
+      return winner;
+    }, { i: [-1], points: 0 });
+
+    this.players.forEach((player, i) => {
+      if (winner.i.includes(i)) player.handleGameVictory();
+      else player.handleGameLoss();
+    });
+  }
+}
+
+export class DrawableGame extends Game implements Drawable {
+  declare players: DrawablePlayer[];
+  declare cardsPlayedPerRound: DrawableCard[][];
+
+  lineLength: number;
+  playerSpacer = "  ";
+
+  constructor(
+    playerCount = 2,
+    public PlayerType = DrawablePlayer,
+    public CardType = DrawableCard,
+  ) {
+    super(playerCount, PlayerType, CardType);
+
+    const playerSpacersTotalLength = (playerCount - 1) *
+      this.playerSpacer.length;
+
+    const playerSpotsTotalLength = playerCount *
+      this.PlayerType.sprite.props.slot.length;
+
+    this.lineLength = playerSpotsTotalLength + playerSpacersTotalLength;
+  }
+
+  static drawTitle(lineLength = 0) {
+    const usableLength = lineLength > this.title.length
+      ? lineLength
+      : this.title.length;
+    const padLength = Math.floor((usableLength - this.title.length) / 2);
+
+    return [
+      Array(usableLength).fill("-").join(""),
+      Array(padLength).fill(" ").concat(this.title).join(""),
+      Array(usableLength).fill("-").join(""),
+    ].join("\n");
+  }
+
+  private drawTitle() {
+    return DrawableGame.drawTitle(this.lineLength);
+  }
+
+  private drawPlayers() {
+    const playerModels = this.players.map((player) => player.draw());
+    const rows = this.PlayerType.sprite.model.split("\n");
+
+    for (const row of rows.keys()) {
+      rows[row] = playerModels.map(
+        (p) => p.split("\n")[row],
+      ).join(this.playerSpacer);
+    }
+
+    return rows.join("\n");
+  }
+
+  drawRound() {
+    return [
+      this.drawTitle(),
+      "",
+      this.drawPlayers(),
+      "",
+      `Cards left: ${this.players[0].handSize}`,
+    ].join(
+      "\n",
+    );
+  }
+
+  drawEndGameResults() {
+    return [
+      this.drawTitle(),
+      "",
+      this.drawPlayers(),
+      "",
+      `Cards left: ${this.players[0].handSize}`,
+    ].join(
+      "\n",
+    );
+  }
+
   draw() {
+    if (this.finished) return this.drawEndGameResults();
+    return this.drawRound();
   }
 }
