c7b5e650668d0204394ed0f342c6c3079b5bcba3
Iury Piva <iurypiva@gmail.com>
2022-01-23 14:58:24 -0300

    visual tests

--- drawable.test.ts
+++ drawable.test.ts
@@ -1,34 +1,53 @@
 import { assertEquals } from "https://deno.land/std@0.122.0/testing/asserts.ts";
 import { DrawableGame } from "./game.ts";
 
-Deno.test("DrawableGame", () => {
-  const baselineDrawTitle0 = `------------
-DENO-UR-CARD
-------------`;
+const baselinesFilePath = new URL("./baselines.json", import.meta.url).pathname;
+
+const baselines: Map<string, string> = new Map(
+  JSON.parse(Deno.readTextFileSync(baselinesFilePath)),
+);
+
+const createBaselines = Deno.args.find((arg) =>
+  arg.startsWith("--create-baseline=")
+)?.replace("--create-baseline=", "");
+
+function imageDiff(key: string, image: string) {
+  const baseline = baselines.get(key);
+
+  if (!baseline) {
+    if (createBaselines?.split(",").some((arg) => arg == key)) {
+      baselines.set(key, image);
+
+      Deno.writeTextFileSync(
+        baselinesFilePath,
+        JSON.stringify([...baselines.entries()]),
+      );
+
+      return;
+    } else {
+      throw new Error(
+        `Baseline "${key}" is missing, to create a new baseline run with "--create-baseline=${key}"`,
+      );
+    }
+  }
+
   assertEquals(
-    baselineDrawTitle0,
+    baseline,
+    image,
+  );
+}
+
+Deno.test("Visual tests", () => {
+  imageDiff(
+    "drawTitle0",
     DrawableGame.drawTitle(0),
   );
 
-  const baselineDrawTitle30 = `------------------------------
-         DENO-UR-CARD
-------------------------------`;
-  assertEquals(
-    baselineDrawTitle30,
+  imageDiff(
+    "drawTitle30",
     DrawableGame.drawTitle(30),
   );
 
-  const firstRoundBaseline = [
-    "--------------",
-    " DENO-UR-CARD",
-    "--------------",
-    "",
-    "0️⃣ 0️⃣ 0️⃣   0️⃣ 0️⃣ 0️⃣ ",
-    "              ",
-    "  😊      😊  ",
-    "",
-    "Cards left: 26",
-  ].join("\n");
   const game = new DrawableGame();
-  assertEquals(firstRoundBaseline, game.draw());
+  imageDiff("firstRoundScreen", game.draw());
 });
